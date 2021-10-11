21d152981811015815169fa9f77e25309d75c2a4
Quentin Forestier <quentin.forestier@heig-vd.ch>
2021-10-11 17:57:34 +0200

    Requete 2 5 et 7 (version 1)

--- lab1/src/main/java/ch/heig/mac/Indices.java
+++ lab1/src/main/java/ch/heig/mac/Indices.java
@@ -12,6 +12,10 @@ public class Indices {
     protected Map<Integer, List<String>> requiredIndices = Map.ofEntries(
             // TODO: For each query, if needed, add the index creation requests
             // Map.entry(<question num>, List.of("CREATE INDEX ...", "CREATE INDEX ..."))
+            Map.entry(7, List.of("CREATE INDEX adv_movie_id ON " +
+                    "`default`:`mflix-sample`.`_default`.`comments`" +
+                    "(`movie_id`)\n" +
+                    "\n"))
     );
 
     public Indices(Cluster cluster) {
--- lab1/src/main/java/ch/heig/mac/Requests.java
+++ lab1/src/main/java/ch/heig/mac/Requests.java
@@ -1,19 +1,23 @@
 package ch.heig.mac;
 
 import java.util.List;
+
 import com.couchbase.client.java.Cluster;
 import com.couchbase.client.java.json.JsonObject;
 import com.couchbase.client.java.query.QueryResult;
 
 
-public class Requests {
+public class Requests
+{
     private final Cluster cluster;
 
-    public Requests(Cluster cluster) {
+    public Requests(Cluster cluster)
+    {
         this.cluster = cluster;
     }
 
-    public List<String> getCollectionNames() {
+    public List<String> getCollectionNames()
+    {
         QueryResult result = cluster.query(
                 "SELECT RAW r.name\n" +
                         "FROM system:keyspaces r\n" +
@@ -22,7 +26,8 @@ public class Requests {
         return result.rowsAs(String.class);
     }
 
-    public List<JsonObject> inconsistentRating() {
+    public List<JsonObject> inconsistentRating()
+    {
         QueryResult result = cluster.query(
                 "SELECT imdb.id imdb_id, tomatoes.viewer.rating " +
                         "tomato_rating, imdb.rating imdb_rating\n" +
@@ -34,51 +39,81 @@ public class Requests {
         return result.rowsAs(JsonObject.class);
     }
 
-    public List<JsonObject> topReviewers() {
-        throw new UnsupportedOperationException("Not implemented, yet");
+    public List<JsonObject> topReviewers()
+    {
+        QueryResult result = cluster.query(
+                "SELECT name, COUNT(name) as cnt\n" +
+                        "FROM `mflix-sample`._default.comments\n" +
+                        "GROUP BY name\n" +
+                        "ORDER BY cnt DESC\n" +
+                        "LIMIT 10"
+        );
+        return result.rowsAs(JsonObject.class);
     }
 
-    public List<String> greatReviewers() {
+    public List<String> greatReviewers()
+    {
         QueryResult result = cluster.query(
                 "SELECT RAW name \n" +
-                         "FROM `mflix-sample`.`_default`.`comments` \n" +
-                         "GROUP BY name \n" +
-                         "HAVING COUNT(_id) > 300;"
+                        "FROM `mflix-sample`.`_default`.`comments` \n" +
+                        "GROUP BY name \n" +
+                        "HAVING COUNT(_id) > 300;"
         );
         return result.rowsAs(String.class);
     }
 
-    public List<JsonObject> bestMoviesOfActor(String actor) {
+    public List<JsonObject> bestMoviesOfActor(String actor)
+    {
         QueryResult result = cluster.query(
                 "SELECT imdb.id, imdb.rating, `cast` \n" +
-                         "FROM `mflix-sample`.`_default`.`movies` \n" +
-                         "WHERE \"" + actor + "\" IN `cast` AND imdb.rating > 9;"
+                        "FROM `mflix-sample`.`_default`.`movies` \n" +
+                        "WHERE \"" + actor + "\" IN `cast` AND imdb.rating > 9;"
         );
         return result.rowsAsObject();
     }
 
-    public List<JsonObject> plentifulDirectors() {
+    public List<JsonObject> plentifulDirectors()
+    {
+        QueryResult result = cluster.query(
+                "SELECT director_name, COUNT(1) count_film\n" +
+                        "FROM `mflix-sample`._default.movies\n" +
+                        "UNNEST directors director_name\n" +
+                        "GROUP BY director_name"
+        );
+
+        return result.rowsAs(JsonObject.class);
+    }
+
+    public List<JsonObject> confusingMovies()
+    {
         throw new UnsupportedOperationException("Not implemented, yet");
     }
 
-    public List<JsonObject> confusingMovies() {
-        throw new UnsupportedOperationException("Not implemented, yet");
+    public List<JsonObject> commentsOfDirector1(String director)
+    {
+        QueryResult result = cluster.query(
+                "SELECT movies._id movie_id, comments.text\n" +
+                        "FROM `mflix-sample`._default.movies\n" +
+                        "JOIN `mflix-sample`._default.comments ON movies._id " +
+                        "= comments.movie_id\n" +
+                        "WHERE \"" + director + "\" IN movies.directors"
+        );
+        return result.rowsAs(JsonObject.class);
     }
 
-    public List<JsonObject> commentsOfDirector1(String director) {
-        throw new UnsupportedOperationException("Not implemented, yet");
-    }
-
-    public List<JsonObject> commentsOfDirector2(String director) {
+    public List<JsonObject> commentsOfDirector2(String director)
+    {
         throw new UnsupportedOperationException("Not implemented, yet");
     }
 
     // Returns true if the update was successful.
-    public Boolean removeEarlyProjection(String movieId) {
+    public Boolean removeEarlyProjection(String movieId)
+    {
         throw new UnsupportedOperationException("Not implemented, yet");
     }
 
-    public List<JsonObject> nightMovies() {
+    public List<JsonObject> nightMovies()
+    {
         throw new UnsupportedOperationException("Not implemented, yet");
     }
 
